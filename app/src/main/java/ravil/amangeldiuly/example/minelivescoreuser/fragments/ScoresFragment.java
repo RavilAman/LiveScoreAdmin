d52dc6519f5cad4a7303fa0a83b35b38855b652d
Ravil Amangeldiuly <ravil.aman.job@gmail.com>
2023-05-25 21:22:18 +0600

    mwp

--- app/src/main/java/ravil/amangeldiuly/example/minelivescoreuser/fragments/ScoresFragment.java
+++ app/src/main/java/ravil/amangeldiuly/example/minelivescoreuser/fragments/ScoresFragment.java
@@ -1,13 +1,18 @@
 package ravil.amangeldiuly.example.minelivescoreuser.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.gson.Gson;
@@ -34,25 +39,27 @@ public class ScoresFragment extends Fragment {
 
     private View currentView;
     private Retrofit retrofit;
-    private Button liveButton;
-    private Button dateButton1;
-    private Button dateButton2;
-    private Button dateButton3;
-    private Button dateButton4;
-    private Button dateButton5;
-    private List<String> daysButtonsText;
-    private List<Button> daysButtons;
+    private List<LocalDate> daysButtonsText;
     private GameApi gameApi;
     private String selectedDate;
+    private Context context;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         currentView = inflater.inflate(R.layout.fragment_scores, container, false);
-
+        radioGroup = currentView.findViewById(R.id.scores_page_days_button_radio_group);
+        context = getContext();
         daysButtonsText = new ArrayList<>();
-        daysButtons = new ArrayList<>();
 
+        getDays(LocalDate.now());
+        setDaysToButtons();
+
+        radioGroup.setOnCheckedChangeListener(radioGroupListener());
+        radioButton = currentView.findViewById(R.id.scores_page_radio_button_3);
+        radioButton.setChecked(true);
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
@@ -63,76 +70,89 @@ public class ScoresFragment extends Fragment {
                 .build();
         gameApi = retrofit.create(GameApi.class);
 
-        dateButton1 = currentView.findViewById(R.id.scores_page_button_1);
-        dateButton2 = currentView.findViewById(R.id.scores_page_button_2);
-        dateButton3 = currentView.findViewById(R.id.scores_page_button_3);
-        dateButton4 = currentView.findViewById(R.id.scores_page_button_4);
-        dateButton5 = currentView.findViewById(R.id.scores_page_button_5);
-
-        daysButtons.add(dateButton1);
-        daysButtons.add(dateButton2);
-        daysButtons.add(dateButton3);
-        daysButtons.add(dateButton4);
-        daysButtons.add(dateButton5);
-
-        dateButton1.setOnClickListener(daysButtonsClickListener());
-
-        getCurrentDate(LocalDate.now());
-        setDaysButtonText();
-
         return currentView;
     }
 
-    private void getCurrentDate(LocalDate date) {
-        StringBuilder buttonDateText = new StringBuilder();
+    private RadioGroup.OnCheckedChangeListener radioGroupListener() {
+        return (group, checkedId) -> {
+            RadioButton radioButton = currentView.findViewById(checkedId);
+            Toast.makeText(context, "Selected date for request: " + formatDateForRequest(
+                    daysButtonsText.get(
+                            getCheckedRadioButtonPosition(radioButton.getId())
+                    )
+            ), Toast.LENGTH_SHORT).show();
+            System.out.println("Selected date for request: " + formatDateForRequest(
+                    daysButtonsText.get(
+                            getCheckedRadioButtonPosition(radioButton.getId())
+                    )));
+            Toast.makeText(context, "Selected date: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
+            System.out.println("Selected date: " + radioButton.getText());
+        };
+    }
+
+    private void getDays(LocalDate date) {
         LocalDate forSave = LocalDate.of(date.getYear(), date.getMonth(), date.getDayOfMonth());
         for (int i = -2; i < 3; i++) {
-            buttonDateText.setLength(0);
             date = date.plusDays(i);
-            buttonDateText.append(date.format(DateTimeFormatter.ofPattern("EEE")).toUpperCase());
-            buttonDateText.append("\n");
-            buttonDateText.append(date.getDayOfMonth());
-            buttonDateText.append(" ");
-            buttonDateText.append(date.format(DateTimeFormatter.ofPattern("MMM")).toUpperCase());
-            daysButtonsText.add(buttonDateText.toString());
+            daysButtonsText.add(date);
             date = forSave;
         }
     }
 
-    private void setDaysButtonText() {
-        for (int i = 0; i < daysButtons.size(); i++) {
-            daysButtons.get(i)
-                    .setText(daysButtonsText.get(i));
+    private void setDaysToButtons() {
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            RadioButton radioButton = (RadioButton) radioGroup.getChildAt(i);
+            radioButton.setText(
+                    formatDaysButtonText(daysButtonsText.get(i))
+            );
         }
     }
 
-    private View.OnClickListener daysButtonsClickListener() {
-        return view -> {
-            gameApi.getGamesByDate(selectedDate).enqueue(new Callback<>() {
-                @Override
-                public void onResponse(Call<List<NewGameDTO>> call, Response<List<NewGameDTO>> response) {
+    private String formatDaysButtonText(LocalDate date) {
+        StringBuilder buttonDateText = new StringBuilder();
+        buttonDateText.append(date.format(DateTimeFormatter.ofPattern("EEE")).toUpperCase());
+        buttonDateText.append("\n");
+        buttonDateText.append(date.getDayOfMonth());
+        buttonDateText.append(" ");
+        buttonDateText.append(date.format(DateTimeFormatter.ofPattern("MMM")).toUpperCase());
+        return buttonDateText.toString();
+    }
 
-                }
+    private int getCheckedRadioButtonPosition(int id) {
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            if (radioGroup.getChildAt(i).getId() == id) {
+                return i;
+            }
+        }
+        return 2;
+    }
 
-                @Override
-                public void onFailure(Call<List<NewGameDTO>> call, Throwable t) {
-                    t.printStackTrace();
-                }
-            });
-        };
+    private void checkDates() {
+        System.out.println("########################################################################################################################");
+
+        daysButtonsText.forEach(date -> System.out.println(formatDaysButtonText(date)));
+
+        System.out.println("########################################################################################################################");
     }
 
 
+    private void setData() {
+        gameApi.getGamesByDate(selectedDate).enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<List<NewGameDTO>> call, Response<List<NewGameDTO>> response) {
+                Log.d("Retrieved data", response.toString());
+            }
 
+            @Override
+            public void onFailure(Call<List<NewGameDTO>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
 
-
-
-
-
-
-
-
-
+    private String formatDateForRequest(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
 
 
 
@@ -163,62 +183,61 @@ public class ScoresFragment extends Fragment {
      previousMonthButton.setOnClickListener(previousMonthAction());
      nextMonthButton.setOnClickListener(nextMonthAction());
 
-    private void initWidgets() {
-        calendarRecyclerView = currentView.findViewById(R.id.calendar_recycler_view);
-        monthText = currentView.findViewById(R.id.current_month_text);
-    }
+     private void initWidgets() {
+     calendarRecyclerView = currentView.findViewById(R.id.calendar_recycler_view);
+     monthText = currentView.findViewById(R.id.current_month_text);
+     }
 
-    private void setMonthView() {
-        monthText.setText(monthFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 7);
-        calendarRecyclerView.setLayoutManager(layoutManager);
-        calendarRecyclerView.setAdapter(calendarAdapter);
-    }
+     private void setMonthView() {
+     monthText.setText(monthFromDate(selectedDate));
+     ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+     CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+     RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 7);
+     calendarRecyclerView.setLayoutManager(layoutManager);
+     calendarRecyclerView.setAdapter(calendarAdapter);
+     }
 
-    private ArrayList<String> daysInMonthArray(LocalDate date) {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-        int daysInMonth = yearMonth.lengthOfMonth();
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+     private ArrayList<String> daysInMonthArray(LocalDate date) {
+     ArrayList<String> daysInMonthArray = new ArrayList<>();
+     YearMonth yearMonth = YearMonth.from(date);
+     int daysInMonth = yearMonth.lengthOfMonth();
+     LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+     int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
-        for (int i = 1; i <= 42; i++) {
-            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
-                daysInMonthArray.add("");
-            } else {
-                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
-            }
-        }
-        return daysInMonthArray;
-    }
+     for (int i = 1; i <= 42; i++) {
+     if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+     daysInMonthArray.add("");
+     } else {
+     daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+     }
+     }
+     return daysInMonthArray;
+     }
 
-    private String monthFromDate(LocalDate date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM");
-        return date.format(formatter);
-    }
+     private String monthFromDate(LocalDate date) {
+     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM");
+     return date.format(formatter);
+     }
 
-    public View.OnClickListener previousMonthAction() {
-        return view -> {
-            selectedDate = selectedDate.minusMonths(1);
-            setMonthView();
-        };
-    }
+     public View.OnClickListener previousMonthAction() {
+     return view -> {
+     selectedDate = selectedDate.minusMonths(1);
+     setMonthView();
+     };
+     }
 
-    public View.OnClickListener nextMonthAction() {
-        return view -> {
-            selectedDate = selectedDate.plusMonths(1);
-            setMonthView();
-        };
-    }
+     public View.OnClickListener nextMonthAction() {
+     return view -> {
+     selectedDate = selectedDate.plusMonths(1);
+     setMonthView();
+     };
+     }
 
-    @Override
-    public void onItemClick(int position, String dayText) {
-        if (!dayText.equals("")) {
-            String message = "Selected Date " + dayText + " " + monthFromDate(selectedDate);
-            Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
-        }
-    }
-    */
+     @Override public void onItemClick(int position, String dayText) {
+     if (!dayText.equals("")) {
+     String message = "Selected Date " + dayText + " " + monthFromDate(selectedDate);
+     Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+     }
+     }
+     */
 }
