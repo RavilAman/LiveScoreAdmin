bf83b974955ad0ea4dc64c1d007a9d222b85637f
Arsensio <100180501+Arsensio@users.noreply.github.com>
2023-06-03 18:08:26 +0600

    add TournamentLisFragment

---
+++ app/src/main/java/ravil/amangeldiuly/example/minelivescoreuser/tournaments/TournamentListAdapter.java
@@ -0,0 +1,54 @@
+package ravil.amangeldiuly.example.minelivescoreuser.tournaments;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import ravil.amangeldiuly.example.minelivescoreuser.R;
+import ravil.amangeldiuly.example.minelivescoreuser.web.responses.TournamentDto;
+
+public class TournamentListAdapter extends RecyclerView.Adapter<TournamentListViewHolder> {
+
+    private Context context;
+    private List<TournamentDto> tournaments;
+    private final TournamentAdapter.OnItemListener onItemListener;
+
+    public TournamentListAdapter(Context context, List<TournamentDto> tournaments, TournamentAdapter.OnItemListener onItemListener) {
+        this.context = context;
+        this.tournaments = tournaments;
+        this.onItemListener = onItemListener;
+    }
+
+    @NonNull
+    @Override
+    public TournamentListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.tournament_admin_item, parent, false);
+        return new TournamentListViewHolder(view, onItemListener, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TournamentListViewHolder holder, int position) {
+        TournamentDto currentTournament = tournaments.get(position);
+        holder.tournamentName.setText(currentTournament.getTournamentName());
+        holder.tournamentLocation.setText(currentTournament.getTournamentLocation());
+        holder.tournamentDto = currentTournament;
+
+        Glide.with(context)
+                .load(currentTournament.getTournamentLogo())
+                .into(holder.tournamentLogo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tournaments.size();
+    }
+}
